18efaf235ce8c0e6a235c1ff711b0461905ae9d0
Pavel <89654104202@mail.ru>
2021-09-19 14:19:51 +0300

    Исправлено

--- src/Main8.java
+++ src/Main8.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main8 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Введите любое целое число");
-        int n = (int) Double.parseDouble(s.next()
-                .replace("-","")
-                .replace(",","."));
+        System.out.println("Введите любое целое положительное число");
+        int n = s.nextInt();
+        for (int x = 0; x >= n;) {
+            System.out.println("Проверьте вводимое значение. Является ли оно целым положительным числом?" + "\n" + "Введите число заново");
+            n = s.nextInt();
+        }
         int sum = 0;
         for (int i = 1; i < n; i = i + 2) {
             sum += i;
